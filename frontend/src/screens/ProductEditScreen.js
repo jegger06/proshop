d1fdfeee1ac22943b72bd28074189d21abf29a80
Jegger Saren <jeggersaren@gmail.com>
2020-11-24 03:20:24 +0800

    Created the ProductEditScreen

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,160 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
+
+const ProductEditScreen = ({ match, history }) => {
+  const productId = match.params.id
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState('')
+  const [brand, setBrand] = useState('')
+  const [category, setCategory] = useState('')
+  const [countInStock, setCountInStock] = useState(0)
+  const [description, setDescription] = useState('')
+
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector(state => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  useEffect(() => {
+    if (!product.name || product._id !== productId) {
+      dispatch(listProductDetails(productId))
+    } else {
+      setName(product.name)
+      setPrice(product.price)
+      setImage(product.image)
+      setBrand(product.brand)
+      setCategory(product.category)
+      setCountInStock(product.countInStock)
+      setDescription(product.description)
+    }
+  }, [dispatch, history, productId, product])
+
+  const onChangeHandler = e => {
+    const setInput = {
+      name: setName,
+      price: setPrice,
+      image: setImage,
+      brand: setBrand,
+      category: setCategory,
+      countInStock: setCountInStock,
+      description: setDescription,
+    }
+
+    setInput[e.target.name](e.target.value)
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    // Update Product
+  }
+
+  return (
+    <>
+      <Link to='/admin/productlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter name'
+                name='name'
+                value={name}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='price'>
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter price'
+                name='price'
+                value={price}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='image'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter image url'
+                name='image'
+                value={image}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='brand'>
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter Brand'
+                name='brand'
+                value={brand}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='countInStock'>
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter Count In Stock'
+                name='countInStock'
+                value={countInStock}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='category'>
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter Category'
+                name='category'
+                value={category}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='description'>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter Description'
+                name='description'
+                value={description}
+                onChange={onChangeHandler}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
+}
+
+export default ProductEditScreen
