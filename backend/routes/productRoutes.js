9827a3cba3fd7963ac81d3f43a8ded03e5724ac5
Jegger Saren <jeggersaren@gmail.com>
2020-11-28 01:54:38 +0800

    Implemented a carousel on top rated products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,12 +7,14 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
