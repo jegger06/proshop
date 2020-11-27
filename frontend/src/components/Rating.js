2c8009db31d2df79b28a59b3c708ea01cff74226
Jegger Saren <jeggersaren@gmail.com>
2020-11-28 00:48:37 +0800

    Implemented the product review

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -45,7 +45,7 @@ const Rating = ({ value, text, color }) => {
           className={
             value >= 4
               ? 'fas fa-star'
-              : value >= 4.5
+              : value >= 3.5
               ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
@@ -75,7 +75,7 @@ Rating.defaultProps = {
 
 Rating.propTypes = {
   value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   color: PropTypes.string,
 }
 
