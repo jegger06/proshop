ce9bf33ca96923e731a16d3a3f61a07c2906a619
Jegger Saren <jeggersaren@gmail.com>
2020-11-28 02:30:05 +0800

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,6 @@ if (process.env.NODE_ENV === 'development') {
 // bodyparser
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
-
 // initialize routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -42,6 +38,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 // 404 middleware
 app.use(notFound)
